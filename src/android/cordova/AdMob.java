5010ce9463538cd6866e60403966650b7e74a719
llapps <lauhoffo@gmail.com>
2022-03-11 12:58:53 +0100

    Update AdMob.java

--- src/android/cordova/AdMob.java
+++ src/android/cordova/AdMob.java
@@ -5,6 +5,10 @@ import android.content.res.Configuration;
 import android.util.Log;
 
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
